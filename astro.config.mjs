6827e6365fabee136d2dec88c35984acd500cd29
BradGreig <brad.s.greig@gmail.com>
2024-07-01 19:35:30 +1000

    Fix missing comma

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://bradgreig.github.io/'
+  site: 'https://bradgreig.github.io/',
   integrations: [tailwind()]
 });
